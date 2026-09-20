ee2a7921e724b70b647f0742a9625c270b96e09d
Forever985 <184053786@qq.com>
2026-09-20 18:27:20 +0800

    deploy: 2026-09-20 18:27:20

--- types/auto/components.d.ts
+++ types/auto/components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElAside: typeof import('element-plus/es')['ElAside']
     ElBacktop: typeof import('element-plus/es')['ElBacktop']
-    ElBadge: typeof import('element-plus/es')['ElBadge']
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
